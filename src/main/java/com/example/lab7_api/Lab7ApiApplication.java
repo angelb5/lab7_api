d96f38c8c4e9e5ff5c5c581aee25a4fb01c3c765
Angel <a20190192@pucp.edu.pe>
2022-06-07 19:29:35 -0500

    Initial commit

---
+++ src/main/java/com/example/lab7_api/Lab7ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab7_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab7_api/Lab7ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
